0534ec5639316f460223945ed3e839915c49abe4
CranezzZ <74801811+CranezzZ@users.noreply.github.com>
2020-11-28 14:11:50 +0800

    Create 493.py

---
+++ 493.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        import bisect
+        arr = []
+        res = 0
+        for num in nums:
+            res += len(arr) - bisect.bisect_right(arr, num * 2)
+            loc = bisect.bisect_right(arr, num)
+            arr[loc:loc] = [num]
+        return res
